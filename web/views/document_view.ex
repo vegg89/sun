00620b3522d6b485d997e5ed48cd17d27378e1d2
Vincent Gutierrez <vincentgg89@gmail.com>
2016-07-25 13:03:23 -0500

    Add control templates

--- web/views/document_view.ex
+++ web/views/document_view.ex
@@ -7,20 +7,19 @@ defmodule Sun.DocumentView do
     |> generate_form(conn)
   end
 
-
   defp generate_form(document, conn) do
     render "form.html", conn: conn, document: document
   end
 
-  def generate_element_section(%Sun.XSD.Element{complex_type: complex_type, name: name, max_ocurrs: "unbounded"}) do
-    render "table.html", element: %Sun.XSD.Element{complex_type: complex_type, name: name}
+  def generate_element_section(%{complex_type: complex_type, name: name, max_ocurrs: "unbounded"}) do
+    render "table.html", element: %{complex_type: complex_type, name: name}
   end
 
-  def generate_element_section(%Sun.XSD.Element{complex_type: complex_type, name: name}) do
-    render "single.html", element: %Sun.XSD.Element{complex_type: complex_type, name: name}
+  def generate_element_section(%{complex_type: complex_type, name: name}) do
+    render "single.html", element: %{complex_type: complex_type, name: name}
   end
 
-  def generate_panel_heading(%Sun.XSD.Element{max_ocurrs: "unbounded", name: name}) do
+  def generate_panel_heading(%{max_ocurrs: "unbounded", name: name}) do
     title = content_tag(:h3, class: "panel-title pull-left", style: "padding-top: 7.5px;") do
       name
     end
@@ -30,9 +29,43 @@ defmodule Sun.DocumentView do
     {:safe, [elem(title,1)|elem(button,1)]}
   end
 
-  def generate_panel_heading(%Sun.XSD.Element{name: name}) do
+  def generate_panel_heading(%{name: name}) do
     content_tag(:h3, class: "panel-title pull-left") do
       name
     end
+  end
+
+  #HTML controls generators
+  #Textbox
+  def generate_control(%{name: name, simple_type: %{base: "xs:string", enumerations: []}, fixed: nil}, parent_name) do
+    render "textbox.html", name: name, parent_name: parent_name
+  end
+  #$ Amount
+  def generate_control(%{name: name, simple_type: %{base: "xs:decimal", enumerations: []}, fixed: nil}, parent_name) do
+    render "amountbox.html", name: name, parent_name: parent_name
+  end
+  #DatePicker
+  def generate_control(%{name: name, simple_type: %{base: "xs:date", enumerations: []}, fixed: nil}, parent_name) do
+    render "datepicker.html", name: name, parent_name: parent_name
+  end
+  #DateTimePicker
+  def generate_control(%{name: name, simple_type: %{base: "xs:dateTime", enumerations: []}, fixed: nil}, parent_name) do
+    render "datetimepicker.html", name: name, parent_name: parent_name
+  end
+  #Number Textbox
+  def generate_control(%{name: name, simple_type: %{base: base, enumerations: []}, fixed: nil}, parent_name) when base == "xs:integer" or base == "xs:int" do
+    render "numberbox.html", name: name, parent_name: parent_name
+  end
+  #Combobox
+  def generate_control(%{name: name, simple_type: %{enumerations: enums}, fixed: nil}, parent_name) do
+    options =
+      for enum <- enums do
+        {"#{enum}", enum}
+      end
+    render "combobox.html", name: name, parent_name: parent_name, options: options
+  end
+  #Hidden
+  def generate_control(%{name: name, fixed: value }, parent_name) do
+    hidden_input(String.to_atom(parent_name), String.to_atom(name), value: value)
   end
 end
